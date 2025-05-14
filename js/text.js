cc04fe9a6f5dab98236d8a7f27ee37ee88f5eb24
marufa181 <marufaakter0996@gmail.com>
2025-05-14 07:04:20 -0400

    global and local variable in global block and function block

--- js/text.js
+++ js/text.js
@@ -13,3 +13,15 @@ function myFunction(a,b){
     return a * b;
 }
 let x = myFunction(5, 6);
+console.log(x); // print the value of x
+
+let carName = "BMW"; // global variable
+function myFunction(){
+    let carName = "Volvo"; // local variable
+    console.log(carName);
+}
+myFunction(); // call the function
+
+console.log(carName); // print the value of carName
+
+
