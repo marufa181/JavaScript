5935a45b84385937bfb2fb4c59b45d300d1134d5
marufa181 <marufaakter0996@gmail.com>
2025-05-14 06:23:05 -0400

    define a function

---
+++ js/text.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+// define a function 
+function sleep(name,time) {
+    console.log(name + " Sleeping my home for " + time + " hours");
+}
+sleep("Marufa", "10 PM"); // call the function
+sleep("Jahan", "8 PM"); 
+sleep("Ms.jahan", "9 PM"); 
