4c58d4d448f0452a02fa4cd1ad8e5d62066312f3
marufa181 <marufaakter0996@gmail.com>
2025-05-19 12:04:32 +0600

    String tamplate Literals

---
+++ js/string_tamplate.js
@@ -0,0 +1,12 @@
+let temp =`Bangladesh is a small country`; // tamplate literal: all benifits of string
+console.log(temp);
+
+let country = "Bangladesh";
+let temp1 = 17;
+let temp2 = 18;
+let sentence = `I love ${country} very much. it is a small country.
+it's population is ${temp1 + temp2} crore`; // using template iteral
+
+// let sentence = 'I love ' + country + ' very much. it is a small country.'; // using string concatenation
+
+console.log(sentence);
